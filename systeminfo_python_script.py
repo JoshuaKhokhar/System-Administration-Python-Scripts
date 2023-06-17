6df69d6f5fc7392514d4a6e985bd8de12b3caf5f
JoshuaKhokhar <131722135+JoshuaKhokhar@users.noreply.github.com>
2023-06-17 18:11:49 -0400

    Add files via upload

---
+++ systeminfo_python_script.py
@@ -0,0 +1,41 @@
+import csv
+import subprocess
+import socket
+from datetime import datetime
+
+def get_system_info():
+    # Run the systeminfo command and capture the output
+    result = subprocess.run('systeminfo', capture_output=True, text=True)
+
+    # Extract relevant system information
+    lines = result.stdout.split('\n')
+    info = []
+    for line in lines:
+        if ':' in line:
+            key, value = line.split(':', 1)
+            info.append([key.strip(), value.strip()])
+
+    return info
+
+def write_to_csv(info, filename):
+    # Write the system info to the CSV file
+    with open(filename, 'w', newline='', encoding='utf-16') as file:
+        writer = csv.writer(file)
+        for row in info:
+            writer.writerow(row)
+
+# Retrieve system information
+system_info = get_system_info()
+
+# Get the hostname
+hostname = socket.gethostname()
+
+# Get the current timestamp
+timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+# Construct the CSV filename using hostname and timestamp
+filename = f'{hostname}_{timestamp}.csv'
+
+# Write system information to the CSV file
+write_to_csv(system_info, filename)
+print(f"System information has been written to {filename}.")
