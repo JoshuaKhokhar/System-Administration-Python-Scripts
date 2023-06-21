136b41a3a5c44be75e99f0a2469acbd752696261
JoshuaKhokhar <131722135+JoshuaKhokhar@users.noreply.github.com>
2023-06-21 14:16:35 -0400

    Update findAndTerminateProcess.py

--- findAndTerminateProcess.py
+++ findAndTerminateProcess.py
@@ -5,7 +5,7 @@ Creation Date: 6/21/2023
 Purpose: This script finds the process it is provided with and terminates it.
 Instructions: To test this script, open up the Notepad application and run this script.
 If the Notepad application terminates after runnning this script, it is functioning properly.
-To terminate any process using this script, replace the "Notepad.exe" parameter for Name with the process you would like to terminate and run the script.
+To terminate any process using this script, replace the "Notepad.exe" argument for the Name parameter with the process you would like to terminate and run the script.
 """
 
 import wmi
